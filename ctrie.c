dee1c458a0643f90b3c34cf7f364d97dd37c5838
adam-lafontaine <adam.lafontaine@icloud.com>
2019-10-10 21:32:01 -0400

    include value in destroy

--- ctrie.c
+++ ctrie.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "ctrie.h"
 
 static const char KEY_CHARS[] = { '0', '1' };
 
 #define CTRIE_KEY_LENGTH sizeof(KEY_CHARS)
+
+//======= HELPERS ===========================
+
+size_t c_idx(char* c) {
+	return *c - 48;
+}
 
 
 
@@ -34,15 +41,30 @@ ct_node* create_node() {
 }
 
 void destroy_node(ct_node* node) {
+	if (node == NULL)
+		return;
+
 	for (size_t i = 0; i < CTRIE_KEY_LENGTH; ++i)
 		if (node->child[i] != NULL)
 			destroy_node(node->child[i]);
 
+	if (node->value != NULL)
+		free(node->value);
+
 	free(node);
 }
 
-void add_child(ct_node* node) {
+bool has_child(ct_node* node, size_t index){
+	return node->child[index] != NULL;
+}
 
+void add_child_node(ct_node* node, size_t index) {
+	ct_node* child = create_node();
+	child->parent = node;
+
+	destroy_node(node->child[index]);		
+
+	node->child[index] = child;
 }
 
 
@@ -71,16 +93,28 @@ ctrie* ctrie_create() {
 
 
 void ctrie_destroy(ctrie* map) {
-
-	if (map->root != NULL)
-		destroy_node(map->root);
+	if (map->root == NULL)
+		return;
+	
+	destroy_node(map->root);
 
 	free(map);
 }
 
 
-void ctrie_add(ctrie* map, char* word) {
+void ctrie_add(ctrie* map, char* s, ctrie_value_t* value) {
 
+	ct_node* node = map->root;
+
+	while (*s != '\0') {
+		size_t idx = c_idx(s++);
+		if (!has_child(node, idx))
+			add_child_node(node, idx);
+
+		node = node->child[idx];
+	}
+
+	node->value = value;
 }
 
 void ctrie_remove(ctrie* map, char* word) {
@@ -92,9 +126,17 @@ void ctrie_remove(ctrie* map, char* word) {
 ctrie* ctrie_create_test() {
 	ctrie* map = ctrie_create();
 
+	map->root->value = (ctrie_value_t*)malloc(sizeof(ctrie_value_t));
+	if (map->root->value != NULL)
+		*map->root->value = 2;
+
 	size_t i = 0;
-	while (i < CTRIE_KEY_LENGTH)
-		map->root->child[i++] = create_node();
+	while (i < CTRIE_KEY_LENGTH) {
+		ct_node* node = (map->root->child[i++] = create_node());
+		node->value = (ctrie_value_t*)malloc(sizeof(ctrie_value_t));
+		if (node->value != NULL)
+			*node->value = 42;		
+	}		
 
 	return map;
 }
--- ctrie.h
+++ ctrie.h
@@ -11,7 +11,7 @@ ctrie* ctrie_create();
 
 void ctrie_destroy(ctrie* map);
 
-void ctrie_add(ctrie* map, char* word);
+void ctrie_add(ctrie* map, char* word, ctrie_value_t* value);
 
 void ctrie_remove(ctrie* map, char* word);
 
